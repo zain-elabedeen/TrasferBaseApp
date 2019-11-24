b27cde602ad3fc058c48f33e143efb3e30b02cbb
Zain <elabedeen.zain@gmail.com>
2019-11-24 18:50:10 +0100

    Create transfer

---
+++ src/components/CreateTransfer.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const CREATE_TRANSFER_MUTATION = gql`
+  mutation createTrasnfer(
+    $receiverAccountId: ID!,
+    $targetCurrency: String!, 
+    $amount: String!
+  ) {
+    createTransfer(
+      receiverAccountId: $receiverAccountId,
+      targetCurrency: $targetCurrency,
+      amount: $amount
+    ) {
+        id
+        senderAccountId
+        receiverAccountId
+        amount,
+        status
+      }
+}        
+`
+
+class CreateTransfer extends Component {
+  state = {
+    receiverAccountId: '',
+    targetCurrency: '',
+    amount: 0
+  }
+
+  render() {
+    const { receiverAccountId, targetCurrency, amount } = this.state
+    return (
+      <div>
+        <div className="flex flex-column mt3">
+          <input
+            className="mb2"
+            value={receiverAccountId}
+            onChange={e => this.setState({ receiverAccountId: e.target.value })}
+            type="text"
+            placeholder="Receiver account id"
+          />
+          <input
+            className="mb2"
+            value={targetCurrency}
+            onChange={e => this.setState({ targetCurrency: e.target.value })}
+            type="text"
+            placeholder="Target currency"
+          />
+          <input
+            className="mb2"
+            value={amount}
+            onChange={e => this.setState({ amount: e.target.value })}
+            type="number"
+            placeholder="Transfer amount"
+          />
+        </div>
+        <Mutation mutation={CREATE_TRANSFER_MUTATION} variables={{ receiverAccountId, targetCurrency, amount }}>
+          {createTransferMutation => <button onClick={createTransferMutation}>Submit</button>}
+        </Mutation>
+      </div>
+    )
+  }
+}
+
+export default CreateTransfer
