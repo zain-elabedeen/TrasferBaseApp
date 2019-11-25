e25a0efc777688a1762ec8526388b0bb97eca029
Zain <elabedeen.zain@gmail.com>
2019-11-25 12:20:58 +0100

    Style payouts table

--- src/components/PayoutList.js
+++ src/components/PayoutList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Payout from './Payout'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
+import { Table, Button, Row, Col } from 'react-bootstrap'
 
 export const PAYOUT_QUERY = gql`
   {
@@ -22,6 +23,13 @@ export const PAYOUT_QUERY = gql`
             name
           }
         }
+        receiverAccount {
+          id
+          user {
+            id
+            name
+          }
+        }
       }
     }
   }
@@ -36,16 +44,46 @@ class PayoutList extends Component {
           if (error) return <div>Error loading payouts</div>
 
           const payoutsToRender = data.listPayouts
-    
+
           return (
-            <div>
-              {payoutsToRender && payoutsToRender.map(
-                payout => 
-                  <Payout key={payout.id}
-                   payout={payout}
-                  />
-              )}
-            </div>
+            <Col xs={12} md={8}>
+              <Row className="show-grid">
+                <Col xs={12} md={8}>
+                  <h1>
+                    Transactions
+                  </h1>
+                </Col>
+                <Col xs={6} md={4}>
+                  <Button
+                    className='button-large'
+                    bsStyle='primary'
+                    bsSize="large"
+                    onClick={ () => (this.props.history.push(`/create-transfer`))}>
+                    Create Transaction
+                  </Button>
+                </Col>
+              </Row>
+              <Table hover>
+                <thead>
+                  <tr>
+                    <th>From</th>
+                    <th>To</th>
+                    <th>Value</th>
+                    <th>Currency</th>
+                    <th>Created at</th>
+                    <th>Updated at</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {payoutsToRender && payoutsToRender.map(
+                    payout =>
+                    <Payout key={payout.id}
+                      payout={payout}
+                    />
+                  )}
+                </tbody>
+              </Table>
+            </Col>
           )
         }}
       </Query>
