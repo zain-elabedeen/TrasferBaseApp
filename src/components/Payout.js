1ba46dd4035d47c0dbfeef5eb4f06a58038c70a3
Zain <elabedeen.zain@gmail.com>
2019-11-25 12:26:08 +0100

    Style payouts

--- src/components/Payout.js
+++ src/components/Payout.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
+import Moment from 'react-moment';
 
 class Payout extends Component {
+  state = {}
+
   render() {
+    const { payout } = this.props
+
+    const amountColor = payout.amount < 0 ? 'red' : 'green'
+    const fromName = payout.amount > 0 ? payout.transfer.senderAccount.user.name : 'You'
+    const toName = payout.amount < 0 ? payout.transfer.receiverAccount.user.name : 'You'
+
     return (
-      <div>
-        <div>
-          {this.props.payout.amount} ({this.props.payout.currency})
-        </div>
-      </div>
+      <tr>
+        <td>{fromName}</td>
+        <td>{toName}</td>
+        <td><font color={amountColor}>{payout.amount}</font></td>
+        <td>{payout.currency}</td>
+        <td><Moment format="MMM, DD YYYY HH:mm">{payout.createdAt}</Moment></td>
+        <td><Moment format="MMM, DD YYYY HH:mm">{payout.updatedAt}</Moment></td>
+      </tr>
     )
   }
 }
